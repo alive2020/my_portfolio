9f3e22eb4d05e210859ce26a5e32c03694fbcaa1
Ice K <alive.k001@gmail.com>
2020-12-23 00:40:41 +0900

    Menu Overlay

--- views/js/main.js
+++ views/js/main.js
@@ -3,9 +3,9 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectoraAll('.nav-item');
+const navItems = document.querySelectorAll('.nav-item');
 
-// Set Initial State of Menu
+// Set Initial State Of Menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -18,7 +18,7 @@ function toggleMenu() {
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
 
-        //Set  Menu State
+        // Set Menu State
         showMenu = true;
     } else {
         menuBtn.classList.remove('close');
@@ -27,6 +27,7 @@ function toggleMenu() {
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
+        // Set Menu State
         showMenu = false;
     }
 }
